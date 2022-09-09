ae56c1f35959cc86ebbde6f10038a2164a107217
«Rustam-Ledsager» <r.yakupov81@yandex.ru>
2022-09-09 07:48:48 +0300

    correct task_03-2

---
+++ task_03-2/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Написать программу масштабирования фигуры Тут для тех кто далеко улетел, чтобы задавались вершины фигуры списком (одной строкой) 
+например: "(0,0) (2,0) (2,2) (0,2)" коэффициент масштабирования k задавался отдельно - 2 или 4 или 0.5 
+В результате показать координаты, которые получатся. при k = 2 получаем "(0,0) (4,0) (4,4) (0,4)"
+*/
+Console.WriteLine("Input scaling factor :");
+int scalfactor;
+    while (!int.TryParse(Console.ReadLine(), out scalfactor))//проверка что вводится число
+        Console.Write("Неверный ввод! \nВведите значение снова : ");
+
+ Console.WriteLine("Input  :");
+int scalfactor;
+    while (!int.TryParse(Console.ReadLine(), out scalfactor))//проверка что вводится число
+        Console.Write("Неверный ввод! \nВведите значение снова : ");
+
+Console.WriteLine("Input coordinate list :");
+var numbers = Console.ReadLine()
+        .Split(new[] {" "},StringSplitOptions.RemoveEmptyEntries)
+        .Select(Int32.Parse);
+foreach(var n in numbers)
+    Console.WriteLine(n);
+
+/*    
+string koordinate = "";
+int i = 0;
+do
+{
+
+    koordinate = "";
+    while ((Convert.ToString(strokavershin[i]) != " ") && (Convert.ToString(strokavershin[i]) != "(") &&
+            (Convert.ToString(strokavershin[i]) != ")") && (Convert.ToString(strokavershin[i]) != ","))
+/*    {
+        koordinate = koordinate + strokavershin[i].ToString();
+        i++;
+    }
+    int j;
+    if (int.TryParse(koordinate, out j) == true)
+    {
+        int b = Int32.Parse(koordinate);
+        Console.Write(b * scalfactor);
+        Console.Write(strokavershin[i]);
+    }
+    else
+        Console.Write(strokavershin[i]);
+    i++;
+}
+while (i < strokavershin.Length);
+*/
